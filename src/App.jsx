510c9fe1aa0efb97ce6b2660e1e5aca836b3dc3c
Jandrinupgrade <alejandro.suarez@bootcamp-upgrade.com>
2022-06-13 10:29:06 +0200

    Crono react

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+
+import './App.scss';
+import React, {useState, useEffect }  from 'react';
+import Stopwatch from './countdown/Countdown';
+
+function App() {
+  
+  
+  
+  
+  
+  return (
+    <div className="app">
+     <Stopwatch/>
+    
+    </div>
+  );
+}
+
+export default App;
